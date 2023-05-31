5cfd9c34587e8d408d24ae249bca04965fb7c807
Charlie <charlesgles9@gmail.com>
2023-05-31 21:42:02 +0300

    added mkv video streaming

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { FileUtil } from './utils/fileutil'
 import { ImgLoader } from './utils/ImgLoader'
-
+import ffmpeg from 'fluent-ffmpeg'
 const app:Application=express()
 
 app.use(express.json())
@@ -115,21 +115,45 @@ app.get("/video", (req,res)=>{
     if(range){
       const [start,end]=range.replace('bytes=','').trim().split('-')
       //offset from the start of the file
-      const startBytes=parseInt(start,10)
+      const startBytes=parseInt(start)
       //the total target size 
-      const endBytes=end!==''?parseInt(end,10):size-1
+      const endBytes=end!==''?parseInt(end):size-1
       //chunk remaining to load 
       const chunkSize=endBytes-startBytes+1
-      const headers={
-        'Content-Range':`bytes ${startBytes}-${endBytes}/${size}`,
-        'Accept-Ranges':'bytes',
-        //prevent overflow 
-        'Content-Length':Math.min(FileUtil.SIZE_MB,chunkSize),
-        'Content-Type':`video/${file.extension()}`
+      console.log(range)
+      if(file.extension()==="mp4"){
+        const headers={
+          'Content-Range':`bytes ${startBytes}-${endBytes}/${size}`,
+          'Accept-Ranges':'bytes',
+          'Transfer-Encoding':'chunked',
+          //prevent overflow 
+          'Content-Length':Math.min(FileUtil.SIZE_MB,chunkSize),
+          'Content-Type':`video/${file.extension()}`
+        }
+        res.writeHead(206,headers)
+        const fileStream= Path.createReadStreamRanged(path.toString(),{start:startBytes,end:startBytes+FileUtil.SIZE_MB})
+        fileStream.pipe(res)
+      }else if(file.extension()==="mkv"){
+        const decodeVideo=async ()=>{
+        const metadata=await getVideoMetaData(path.toString())
+        if(metadata.format.duration&&metadata.format.bit_rate&&metadata.format.size){
+            const timeStamp=calculateTimestamps(metadata.format.duration,metadata.format.bit_rate,startBytes,startBytes+FileUtil.SIZE_MB*1,metadata.format.size)
+            const output=file.parent().toString()+"/temp.output.mp4"
+            //if(!new Path(output).exists())
+       decodeMKVToMP4(res,file.toString(),output,{startTimestamp:timeStamp.startTimestamp, endTimestamp:timeStamp.endTimestamp,
+        videoDuration:metadata.format.duration, startBytes:startBytes,endBytes:endBytes, size:metadata.format.size}).then(() => {
+             console.log('MKV decoding to MP4 completed successfully.')
+              // Additional logic after successful decoding
+          }).catch((error: Error) => {
+              console.error('MKV decoding to MP4 failed:', error)
+              // Additional error handling
+         })
+        }
       }
-      res.writeHead(206,headers)
-      const fileStream= Path.createReadStreamRanged(path.toString(),{start:startBytes,end:startBytes+FileUtil.SIZE_MB})
-      fileStream.pipe(res)
+        decodeVideo()
+       
+      }
+     
     }else{
   
       res.writeHead(206,{
@@ -139,12 +163,132 @@ app.get("/video", (req,res)=>{
         'Content-Type':`video/${file.extension()}`
       })
 
+      if(file.extension()==="mp4"){
       const fileStream= Path.createReadStreamRanged(path.toString(),{start:0,end:size})
       fileStream.pipe(res)
+      }else if(file.extension()=="mkv"){
+       // decodeAndStreamMKV(file.toString(),file.parent().toString()+"/output.mp4",res,0,size)
+      }
      }
    }
 
 })
+
+
+ function getVideoMetaData(file:string ):Promise<ffmpeg.FfprobeData>{
+
+  return new Promise<ffmpeg.FfprobeData>((resolve, reject)=>{
+    ffmpeg.ffprobe(file, (err, metadata) => {
+      if (err) {
+        console.error('Error:', err.message)
+        return
+      }
+
+     // console.log(metadata)
+      resolve(metadata)
+    
+    })
+  })
+ 
+}
+
+
+function calculateTimestamps(duration: number, bitrate: number, startByte: number, endByte: number,fileSize:number): { startTimestamp: number, endTimestamp: number } {
+ 
+     const bytesPerSecond=(bitrate/8)
+     const startTimestamp=startByte/bytesPerSecond
+     const endTimestamp=endByte/bytesPerSecond
+  return { startTimestamp, endTimestamp };
+}
+
+function decodeMKVToMP4(res:Response,inputPath: string, outputPath: string,options:any): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+
+
+    console.log("start ", options.startTimestamp)
+    console.log("end ", options.endTimestamp)
+    console.log("duration",options.endTimestamp-options.startTimestamp)
+    
+ const command= ffmpeg(Path.createReadStream(inputPath))
+  .seekInput(options.startTimestamp)
+  .setDuration(options.videoDuration)
+  .format("matroska")
+  .outputOptions('-c:v libx264')
+  .outputOptions('-c:a aac')
+  .outputOption('-preset ultrafast')
+  .outputOptions('-movflags frag_keyframe+empty_moov')
+  .on('error', (err) => {
+    console.error('FFmpeg error:', err.message)
+    command.kill('SIGKILL')
+  })
+  .on('end', () => {
+    console.log('Transcoding completed successfully.')
+    command.kill('SIGKILL')
+    res.end()
+    resolve()
+   })
+  
+    res.setHeader('Content-Type', 'video/mp4')
+    res.setHeader('Accept-Ranges','bytes')
+    res.setHeader('Content-Range',`bytes ${options.startBytes}-${options.size}/*`)
+   command.pipe(res)
+  
+  })
+    /*ffmpeg(Path.createReadStream(inputPath))
+    .setStartTime(startTimestamp)
+    .setDuration(endTimestamp)
+    .outputOptions('-c:v libx264')
+    .outputOptions('-c:a aac')
+    .outputOptions('-movflags +faststart')
+    .outputOptions('-strict -2')
+    .format('mp4')
+    .on('error', (err) => {
+      console.error(err)
+      res.end()
+    }).on('close', ()=>{
+      resolve()
+    })
+    .pipe(res)*/
+    
+    /*const ffmpeg = spawn('ffmpeg', [
+      '-ss', `${startTimestamp}`,
+      '-to', `${endTimestamp}`,
+      '-i', inputPath,
+      '-y',
+      '-c:v', 'libx264',
+      '-preset', 'fast',
+      '-crf', '22',
+      '-c:a', 'aac',
+      '-strict', '-2',
+      '-movflags', '+faststart',
+      outputPath
+    ])
+
+    
+    /*'-c:v', 'libx264',
+    '-preset', 'fast',
+    '-crf', '22',
+    '-c:a', 'aac',
+    '-strict', '-2',
+    '-movflags', '+faststart',*/
+   /* ffmpeg.stderr.on('data', (data: Buffer) => {
+      console.error(`FFmpeg stderr: ${data}`)
+    })
+
+    ffmpeg.on('close', (code: number) => {
+      if (code === 0) {
+        const fileStream =Path.createReadStream(outputPath)
+        fileStream.pipe(res)
+        console.log('MKV decoding to MP4 completed successfully.')
+        resolve()
+      } else {
+        reject(new Error(`MKV decoding to MP4 failed with code ${code}`))
+      }
+    })*/
+  
+}
+
+
 
 
 app.listen(PORT,():void=>{
